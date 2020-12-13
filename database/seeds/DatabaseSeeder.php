22c80c8d6c7697664e5e2970f72b024e043c258e
Osman Forhad <osmanforhad93@gmail.com>
2020-12-13 15:26:31 +0600

    working with role package with role and permission seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-    }
-}
+        $this->call(UserSeeder::class);
+        $this->call(RolePermissionSeeder::class);
+    } //end of the run method
+
+} //end of the DatabaseSeeder class
