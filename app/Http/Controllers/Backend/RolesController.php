2a4de01ca6dceb42bb70c04b02d350376f07c8b1
Osman Forhad <osmanforhad93@gmail.com>
2020-12-14 15:02:18 +0600

    working with role creation and permission assign

---
+++ app/Http/Controllers/Backend/RolesController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::all();
+
+        return view('backend.pages.roles.index', compact('roles'));
+    } //end of the index method
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $permissions = Permission::all();
+        return view('backend.pages.roles.create', compact('permissions'));
+    } //end of the create method
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        //data validation
+        $request->validate([
+            'name' => 'required|max:100|unique:roles',
+        ], [
+            'name.required' => 'Please give a role name',
+        ]);
+
+        //data processing
+        $role = Role::create(['name' => $request->name]);
+
+        // $role = DB::table('roles')->where('name', $request->name)->first();
+        $permissions = $request->input('permissions');
+
+        if (!empty($permissions)) {
+            $role->syncPermissions($permissions);
+        }
+        return back();
+
+    } //end of the store method
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+} //end of the RolesController class
--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -7,6 +7,7 @@
     <title>@yield('title', 'Laravel Role Admin')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
    @include('backend.layouts.partials.styles')
+   @yield('styles')
 </head>
 
 <body>
@@ -39,6 +40,8 @@
    @include('backend.layouts.partials.offsets')
 
     @include('backend.layouts.partials.scripts')
+
+    @yield('scripts')
 </body>
 
 </html>
